bc672a2262c5819d5af44fac85e5d504c1768825
akshat <honeyakshat999@gmail.com>
2019-08-06 02:12:09 +0530

    first program

---
+++ atm.pl
@@ -0,0 +1,30 @@
+#! usr/bin/perl
+my $acc_no=123456;
+print "hi welcome";
+print "choose your prefered language.\n1.english\n2.hindi\n3.marathi\n";
+my $lang=<stdin>;
+chomp($lang);
+if($lang==1){
+
+print "plz choose from following option.\n1.money transfer\n2.check balance\n3.change pin\n";
+my $choice=<stdin>;
+chomp($choice);
+if($choice==1){
+
+print "enter how much do u want to take out\n";
+$amount=<stdin>;
+#chomp($amount);
+print "plz enter the 4 digit pin";
+my $pin=<stdin>;
+chomp($pin);
+if($pin==3000)
+{
+print "transection successful";
+}
+else{
+print "sorry, your transection abort due to wrong details";}
+
+}
+}
+
+else{print "tnx,but this is in development";}
